2434704b685001d616e3b3df2b693bce994e7c5e
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-11-09 15:24:41 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <div class="parent w-full h-full">
     <header class="section bg-red">Header</header>
     <div class="left-side section bg-blue">Left Sidebar</div>
-    <main class="section bg-green">Main Content</main>
+    <main class="section bg-green">
+        <div class="h-[100vh]">Main Content</div>
+    </main>
     <div class="right-side section bg-yellow">Right Sidebar</div>
     <footer class="section bg-red">Footer</footer>
 </div>
