4649003abbb09a9e61d262c5926f8ad4364e0675
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-11-25 15:40:21 -0500

    Update About.svelte

--- src/pages/settings/About.svelte
+++ src/pages/settings/About.svelte
@@ -3,6 +3,7 @@
     import { getVersion, getTauriVersion } from '@tauri-apps/api/app';
     import Layout from './comp/Layout.svelte';
     import { git_url } from '$lib/utils/index';
+    import { arch, platform } from '@tauri-apps/api/os';
 
     let py_modules: string[] = [];
 
@@ -11,29 +12,46 @@
         const [err, result] = await oO(axios(`${git_url.py.usercontent()}/src/requirements.txt`));
         if (err) return toast.error(err);
         py_modules = result?.data.split('\n') ?? '';
+
+        system_info.platform = await platform();
+        system_info.arch = await arch();
     });
+
+    const system_info = {
+        platform: '',
+        arch: '',
+    };
 </script>
 
 <Layout id="About" class="pl-5">
     <h1>About</h1>
     <div class="content">
-        <ul style="user-select: text;" class="ml-0">
+        <ul class="ml-0 flex flex-col gap-1">
+            <li>Platform: <span class="badge badge-info">{system_info.platform}-{system_info.arch}</span></li>
+            <hr />
+
             {#await getVersion() then value}
-                <li>UMDA_UI: {value}</li>
+                <li>UMDA_UI: <span class="badge badge-info">{value}</span></li>
             {/await}
 
             {#await getTauriVersion() then value}
-                <li>Tauri: {value}</li>
+                <li>Tauri: <span class="badge badge-info">{value}</span></li>
             {/await}
-            <li>Python {$pyVersion}</li>
-            <li>umdapy {$umdapyVersion}</li>
+            <li>
+                Python: <span class="badge badge-info" class:badge-error={!$pyVersion}>{$pyVersion || 'unknown'}</span>
+            </li>
+            <li>
+                umdapy: <span class="badge badge-info" class:badge-error={!$umdapyVersion}
+                    >{$umdapyVersion || 'unknown'}</span
+                >
+            </li>
             <hr />
 
             {#if $umdapyVersion && py_modules.length > 0}
                 <h3>Python libraries</h3>
                 {#each py_modules as module}
                     {#if module}
-                        <li>{module}</li>
+                        <li><span class="badge badge-info">{module}</span></li>
                     {/if}
                 {/each}
             {/if}
