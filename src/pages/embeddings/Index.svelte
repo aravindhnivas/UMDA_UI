871827e77f4b458338a3831a1ccdd3e0afe7a44b
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-11-25 19:23:57 -0500

    Update Index.svelte

--- src/pages/embeddings/Index.svelte
+++ src/pages/embeddings/Index.svelte
@@ -2,23 +2,12 @@
     import { LoadingBtn } from '$lib/components/';
     import Page from '$lib/layouts/Page.svelte';
     import { Pane } from 'svelte-splitpanes';
-
-    let progress = 0.1;
-    let loading = true;
 </script>
 
 <Page id="Embeddings">
     <svelte:fragment slot="body">
         <Pane class="p-5">
             <h1 class="text-3xl font-bold">Embeddings</h1>
-            <LoadingBtn
-                name="Click me"
-                {loading}
-                on:click={() => {
-                    progress += 0.05;
-                }}
-            />
-            <progress class="progress w-full" value={progress} max="1"></progress>
         </Pane>
     </svelte:fragment>
 </Page>
