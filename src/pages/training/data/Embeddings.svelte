98d2d0e6122f0fd95a29154591efd5b70218ded4
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-11-30 21:44:07 -0500

    Update Embeddings.svelte

--- src/pages/training/data/Embeddings.svelte
+++ src/pages/training/data/Embeddings.svelte
@@ -1,6 +1,26 @@
 <script lang="ts">
     export let columns: string[] = [];
+
+    let df_column = '';
+    const embeddings = ['mol2vec', 'VICGAE'];
+    let embedding = embeddings[0];
 </script>
 
-<div class="container">Embeddings</div>
-{columns}
+<h2>Embeddings</h2>
+<div class="flex gap-1">
+    <select class="select select-sm select-bordered" bind:value={df_column}>
+        <option disabled selected>Column</option>
+        {#each columns as column}
+            <option>{column}</option>
+        {/each}
+    </select>
+
+    <select class="select select-sm select-bordered" bind:value={embedding}>
+        <option disabled selected>embedding</option>
+        {#each embeddings as embed}
+            <option>{embed}</option>
+        {/each}
+    </select>
+
+    <button class="btn btn-sm">Compute</button>
+</div>
