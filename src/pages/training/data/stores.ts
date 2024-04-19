1df3b1d911f05b7b35668492e90495cd68ce32ab
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-04-19 14:54:03 -0400

    Update stores.ts

--- src/pages/training/data/stores.ts
+++ src/pages/training/data/stores.ts
@@ -1,7 +1,12 @@
+import { writable as writable_store } from '@macfja/svelte-persistent-store';
+
 export const embedding = writable<Embedding>('mol2vec');
 export const default_pretrained_modes = writable<{ [key: string]: boolean }>({
     mol2vec: false,
     VICGAE: false,
 });
 
-export const pretrained_model_location = writable<Record<Embedding, string>>({} as Record<Embedding, string>);
+export const pretrained_model_location = writable_store<Record<Embedding, string>>(
+    'pretrained_model_location',
+    {} as Record<Embedding, string>,
+);
