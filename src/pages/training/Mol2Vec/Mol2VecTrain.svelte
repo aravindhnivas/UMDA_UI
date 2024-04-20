f08af74a78b8b883c5acf1499c938dfad387b249
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-04-20 19:49:27 -0400

    Update Mol2VecTrain.svelte

--- src/pages/training/Mol2Vec/Mol2VecTrain.svelte
+++ src/pages/training/Mol2Vec/Mol2VecTrain.svelte
@@ -15,7 +15,7 @@
 
     let vector_size = 300;
     let radius = 1;
-    let n_jobs = 1;
+    let n_jobs = 32;
     let min_count = 1;
 </script>
 
@@ -26,5 +26,33 @@
     <FileLoader bind:filename={$filename} bind:data bind:filetype bind:key filetypes={['csv']} />
 
     <h3>Generate a Mol2Vec embedder model</h3>
-    <BrowseFile bind:filename={corpus_file} btn_name={'Browse corpus_file'} />
+
+    <div class="flex gap-1">
+        <div class="flex flex-col gap-1">
+            <span class="text-xs pl-1">sentence_type</span>
+            <input type="text" class="input input-sm" bind:value={sentence_type} />
+        </div>
+
+        <div class="flex flex-col gap-1">
+            <span class="text-xs pl-1">vector_size</span>
+            <input type="number" class="input input-sm" bind:value={vector_size} />
+        </div>
+
+        <div class="flex flex-col gap-1">
+            <span class="text-xs pl-1">radius</span>
+            <input type="number" class="input input-sm" bind:value={radius} />
+        </div>
+
+        <div class="flex flex-col gap-1">
+            <span class="text-xs pl-1">n_jobs</span>
+            <input type="number" class="input input-sm" bind:value={n_jobs} />
+        </div>
+
+        <div class="flex flex-col gap-1">
+            <span class="text-xs pl-1">min_count</span>
+            <input type="number" class="input input-sm" bind:value={min_count} />
+        </div>
+    </div>
+
+    <BrowseFile bind:filename={corpus_file} btn_name={'Browse corpus_file'} label="Optional" />
 </div>
