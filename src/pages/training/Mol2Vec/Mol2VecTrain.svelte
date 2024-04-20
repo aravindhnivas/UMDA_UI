4b4ca4e763faad6d50875d2c9c1d12097c2eeb26
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-04-20 19:24:02 -0400

    Update Mol2VecTrain.svelte

--- src/pages/training/Mol2Vec/Mol2VecTrain.svelte
+++ src/pages/training/Mol2Vec/Mol2VecTrain.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
+    import FileLoader from './../FileLoader.svelte';
     export let id: string = 'mol2vec-train-container';
     export let display: string = 'none';
+
+    const filename = writable_store('data_filename', '');
+    let data: DataType | null = null;
+    let filetype = 'csv';
+    let key = 'data';
 </script>
 
-<div {id} style:display>
-    <div>Mol2Vec</div>
+<div class="grid content-start gap-2" {id} style:display>
+    <h2>Mol2Vec - create embedder</h2>
+
+    <h3>Load data file</h3>
+    <FileLoader bind:filename={$filename} bind:data bind:filetype bind:key />
+
+    <h3>Generate a Mol2Vec embedder model</h3>
 </div>
