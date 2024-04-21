071f75589d7c5d6b531acc10b6bd8d5f1b7b4e61
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-04-20 20:01:09 -0400

    Update Mol2VecTrain.svelte

--- src/pages/training/Mol2Vec/Mol2VecTrain.svelte
+++ src/pages/training/Mol2Vec/Mol2VecTrain.svelte
@@ -2,6 +2,7 @@
     import { HelpCircle } from 'lucide-svelte/icons';
     import FileLoader from './../FileLoader.svelte';
     import BrowseFile from '$lib/components/BrowseFile.svelte';
+    import CustomSelect from '$lib/components/CustomSelect.svelte';
     export let id: string = 'mol2vec-train-container';
     export let display: string = 'none';
 
@@ -12,7 +13,11 @@
 
     let corpus_file = '';
     let sentence_type = 'alt';
-
+    const sentence_type_info: Record<string, string> = {
+        all: 'generates all corpus files for all types of sentences',
+        alt: 'generates a corpus file with only combined alternating sentence',
+        individual: 'generates corpus files for each radius',
+    };
     let vector_size = 300;
     let radius = 1;
     let n_jobs = 32;
@@ -28,31 +33,42 @@
     <h3>Generate a Mol2Vec embedder model</h3>
 
     <div class="flex gap-1">
+        <CustomSelect
+            label="sentence_type"
+            bind:value={sentence_type}
+            items={['all', 'alt', 'individual']}
+            helper={sentence_type_info[sentence_type]}
+        />
         <div class="flex flex-col gap-1">
-            <span class="text-xs pl-1">sentence_type</span>
-            <input type="text" class="input input-sm" bind:value={sentence_type} />
+            <span class="text-xs pl-1">radius</span>
+            <input type="number" class="input input-sm" bind:value={radius} />
+            <span class="text-xs pl-1 m-auto">Radius of morgan fingerprint</span>
         </div>
 
         <div class="flex flex-col gap-1">
             <span class="text-xs pl-1">vector_size</span>
             <input type="number" class="input input-sm" bind:value={vector_size} />
-        </div>
-
-        <div class="flex flex-col gap-1">
-            <span class="text-xs pl-1">radius</span>
-            <input type="number" class="input input-sm" bind:value={radius} />
-        </div>
-
-        <div class="flex flex-col gap-1">
-            <span class="text-xs pl-1">n_jobs</span>
-            <input type="number" class="input input-sm" bind:value={n_jobs} />
+            <span class="text-xs pl-1 m-auto">Number of dimensions of vector</span>
         </div>
 
         <div class="flex flex-col gap-1">
             <span class="text-xs pl-1">min_count</span>
             <input type="number" class="input input-sm" bind:value={min_count} />
+            <span class="text-xs pl-1 m-auto">
+                Number of occurrences a word should have to be considered in training</span
+            >
+        </div>
+
+        <div class="flex flex-col gap-1">
+            <span class="text-xs pl-1">n_jobs</span>
+            <input type="number" class="input input-sm" bind:value={n_jobs} />
+            <span class="text-xs pl-1 m-auto">Number of cpu cores used for calculation</span>
         </div>
     </div>
 
-    <BrowseFile bind:filename={corpus_file} btn_name={'Browse corpus_file'} label="Optional" />
+    <BrowseFile
+        bind:filename={corpus_file}
+        btn_name={'Browse corpus_file'}
+        label="Optional. Choose corpus file if you have one!"
+    />
 </div>
