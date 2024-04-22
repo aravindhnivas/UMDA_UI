f687ca6e48bd450edaf5711cf4e9a87ec55aa68c
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-04-22 18:09:40 -0400

    Create CustomInput.svelte

---
+++ src/lib/components/CustomInput.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let label: string = '';
+    export let value: string | number;
+    export let helper: string = '';
+    let className = '';
+    export { className as class };
+</script>
+
+<div class="flex flex-col gap-1">
+    {#if label}
+        <span class="text-xs pl-1">{label}</span>
+    {/if}
+
+    <input class="input input-sm {className}" bind:value {...$$restProps} on:change />
+    {#if helper}
+        <span class="text-xs m-auto">{label}</span>
+    {/if}
+</div>
