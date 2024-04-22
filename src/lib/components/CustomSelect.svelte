43bc687c8526100c0787138ed75cc21cc7978e99
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-04-22 18:09:42 -0400

    Update CustomSelect.svelte

--- src/lib/components/CustomSelect.svelte
+++ src/lib/components/CustomSelect.svelte
@@ -3,11 +3,15 @@
     export let value: string;
     export let items: string[] = [];
     export let helper: string = '';
+
+    let className = '';
+    export { className as class };
 </script>
 
 <div class="flex flex-col gap-1 min-w-[100px]">
     <span class="text-xs pl-1">{label}</span>
-    <select class="select select-sm select-bordered" bind:value>
+
+    <select class="select select-sm select-bordered {className}" bind:value>
         {#if !label}
             <option disabled selected>embedding</option>
         {/if}
