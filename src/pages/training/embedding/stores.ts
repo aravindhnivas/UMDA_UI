206217a0ef6bea23e91d63755147ecabc889e4e2
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-04-28 13:14:03 -0400

    Update stores.ts

--- src/pages/training/embedding/stores.ts
+++ src/pages/training/embedding/stores.ts
@@ -1,6 +1,8 @@
+export const embeddings = ['mol2vec', 'mol2vec_PCA', 'VICGAE'];
 export const embedding = writable<Embedding>('mol2vec');
 export const default_pretrained_modes = writable<{ [key: string]: boolean }>({
     mol2vec: false,
+    mol2vec_PCA: false,
     VICGAE: false,
 });
 
