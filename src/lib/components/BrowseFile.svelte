092647eddbc3327dea82ebc284cae1995396a032
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-05-02 17:38:38 -0400

    Update BrowseFile.svelte

--- src/lib/components/BrowseFile.svelte
+++ src/lib/components/BrowseFile.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+    import { isError, tryF } from 'ts-try';
     import { HelpCircle } from 'lucide-svelte/icons';
     export let btn_name = 'Browse file';
     export let filename = '';
     export let helper = '';
     export let label = '';
     export let directory = false;
-    export let load_callback: null | ((filename: string) => void) = null;
+    export let load_callback: null | ((filename: string) => Promise<void>) = null;
 
     const dispatch = createEventDispatcher();
 
@@ -19,6 +20,20 @@
         }
         if (filename) dispatch('file_selected', filename);
     };
+
+    const callback = async () => {
+        try {
+            if (!load_callback) return toast.error('No load callback provided');
+            if (!filename) return toast.error('No file selected');
+            const result = await load_callback(filename);
+            toast.success('File loaded successfully: ' + (await path.basename(filename)));
+            return result;
+        } catch (error) {
+            if (typeof error === 'string') toast.error(error);
+            else if (isError(error)) toast.error(error.message);
+            else toast.error('An error occurred');
+        }
+    };
 </script>
 
 <div class="flex flex-col gap-1">
@@ -29,9 +44,9 @@
     {/if}
     <div class="join">
         <button class="btn btn-sm join-item" on:click={browse_file}>{btn_name}</button>
-        <input type="text" class="input input-sm join-item w-full" bind:value={filename} />
+        <input type="text" class="input input-sm join-item w-full" bind:value={filename} on:change={callback} />
         {#if load_callback}
-            <button class="btn btn-sm join-item" on:click={() => load_callback?.(filename)}>load</button>
+            <button class="btn btn-sm join-item" on:click={callback}>load</button>
         {/if}
     </div>
     {#if helper}
