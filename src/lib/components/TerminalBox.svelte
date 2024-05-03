aaaf1e4f7b7fd01bf58de26cba007c9d0b2915fe
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-05-03 17:22:05 -0400

    Update TerminalBox.svelte

--- src/lib/components/TerminalBox.svelte
+++ src/lib/components/TerminalBox.svelte
@@ -7,9 +7,17 @@
 
     const mount = (node: HTMLElement) => {
         terminal = new LOGGER(node, { fontSize: 14, fontFamily: 'monospace' });
+        terminal.fitAddon.fit();
     };
     onDestroy(() => terminal?.term.dispose());
 </script>
 
-<button class="btn btn-sm ml-auto" on:click={() => terminal?.term.clear()}>Clear</button>
-<div class={className} use:mount></div>
+<button
+    class="btn btn-sm ml-auto"
+    on:click={() => {
+        if (!terminal) return toast.error('Terminal not initialized');
+        terminal.term.clear();
+        terminal.warn('Terminal cleared by user!!');
+    }}>Clear</button
+>
+<div class="{className} w-full" use:mount></div>
