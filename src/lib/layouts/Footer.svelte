e755a8f8640b2102f009806aae3a7ff05130ba60
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-06-27 16:10:09 -0400

    footer notes update

--- src/lib/layouts/Footer.svelte
+++ src/lib/layouts/Footer.svelte
@@ -6,7 +6,19 @@
 
 <div class="footer items-center bg-orange-400 h-[2rem]">
     <aside class="items-center grid-flow-col px-4 w-full">
-        <p class="text-black">{import.meta.env.VITE_YEAR} &copy; {import.meta.env.VITE_AUTHOUR} | MIT Licence</p>
+        <p class="text-black">
+            <span>{import.meta.env.VITE_YEAR} &copy; {import.meta.env.VITE_AUTHOUR} |</span>
+            <!-- svelte-ignore missing-declaration -->
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <!-- svelte-ignore a11y-no-static-element-interactions -->
+            <span
+                on:click={async () => {
+                    await shell.open('https://mcguirelab.mit.edu/index.html');
+                }}
+            >
+                Developed at <span class="underline cursor-pointer">{import.meta.env.VITE_GROUP}</span>
+            </span>
+        </p>
         <div class="flex gap-2 ml-auto">
             <FooterLabel />
             <FooterDownloadStatus />
