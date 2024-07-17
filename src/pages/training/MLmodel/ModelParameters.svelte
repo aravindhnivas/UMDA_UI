6e7bc35f5bf8bbbffbd90b83f7ebbba8cae2f76b
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-07-17 16:29:47 -0400

    Update ModelParameters.svelte

--- src/pages/training/MLmodel/ModelParameters.svelte
+++ src/pages/training/MLmodel/ModelParameters.svelte
@@ -51,60 +51,62 @@
     $: origial_values = get_values_from_model($model);
 </script>
 
-<div class="flex flex-col gap-4 hyperparameters__div">
-    {#each Object.keys(parameters) as label (label)}
-        {@const { value, description } = parameters[label]}
-        {#if typeof value === 'boolean'}
-            <div class="grid gap-1">
-                <Checkbox class="p-2 w-max" bind:value={values[label]} {label} />
-                <span class="text-xs">
-                    {description}
-                    <div class="badge badge-sm badge-neutral">Default: {origial_values[label]}</div>
-                </span>
-            </div>
-        {:else if typeof value === 'string' || typeof value === 'number'}
-            <div class="grid gap-1">
-                <Textfield class="w-max" bind:value={values[label]} {label} autocomplete="false" />
-                <span class="text-xs"
-                    >{description}
-                    <div class="badge badge-sm badge-neutral">Default: {origial_values[label]}</div>
-                </span>
-            </div>
-        {:else if typeof value === 'object' && value}
-            <div class="grid w-max">
-                <div class="flex items-end gap-4">
-                    <CustomSelect
-                        label={`${label} (${value.options[values[label]]})`}
-                        items={Object.keys(value.options)}
-                        bind:value={values[label]}
-                    />
-                    {#if values[label] === 'float'}
-                        <div class="grid">
-                            <span class="text-xs pl-1">Enter {label} value</span>
-                            <!-- svelte-ignore a11y-autocomplete-valid -->
-                            <input
-                                autocomplete="false"
-                                on:keydown={validateInput}
-                                id="{unique_id}_{label}"
-                                class="input input-sm"
-                                type="number"
-                            />
-                        </div>
-                    {/if}
+{#if origial_values}
+    <div class="flex flex-col gap-4 hyperparameters__div">
+        {#each Object.keys(parameters) as label (label)}
+            {@const { value, description } = parameters[label]}
+            {#if typeof value === 'boolean'}
+                <div class="grid gap-1">
+                    <Checkbox class="p-2 w-max" bind:value={values[label]} {label} />
+                    <span class="text-xs">
+                        {description}
+                        <div class="badge badge-sm badge-neutral">Default: {origial_values[label]}</div>
+                    </span>
                 </div>
-                <span class="text-xs pl-1"
-                    >{description}
-                    <div class="badge badge-sm badge-neutral">Default: {origial_values[label]}</div>
-                </span>
-            </div>
-        {:else if value == null}
-            <div class="grid">
-                <Textfield class="w-max" bind:value={values[label]} {label} autocomplete="false" />
-                <span class="text-xs">{description}. Default: None</span>
-            </div>
-        {/if}
-    {/each}
-</div>
+            {:else if typeof value === 'string' || typeof value === 'number'}
+                <div class="grid gap-1">
+                    <Textfield class="w-max" bind:value={values[label]} {label} autocomplete="false" />
+                    <span class="text-xs"
+                        >{description}
+                        <div class="badge badge-sm badge-neutral">Default: {origial_values[label]}</div>
+                    </span>
+                </div>
+            {:else if typeof value === 'object' && value}
+                <div class="grid w-max">
+                    <div class="flex items-end gap-4">
+                        <CustomSelect
+                            label={`${label} (${value.options[values[label]]})`}
+                            items={Object.keys(value.options)}
+                            bind:value={values[label]}
+                        />
+                        {#if values[label] === 'float'}
+                            <div class="grid">
+                                <span class="text-xs pl-1">Enter {label} value</span>
+                                <!-- svelte-ignore a11y-autocomplete-valid -->
+                                <input
+                                    autocomplete="false"
+                                    on:keydown={validateInput}
+                                    id="{unique_id}_{label}"
+                                    class="input input-sm"
+                                    type="number"
+                                />
+                            </div>
+                        {/if}
+                    </div>
+                    <span class="text-xs pl-1"
+                        >{description}
+                        <div class="badge badge-sm badge-neutral">Default: {origial_values[label]}</div>
+                    </span>
+                </div>
+            {:else if value == null}
+                <div class="grid">
+                    <Textfield class="w-max" bind:value={values[label]} {label} autocomplete="false" />
+                    <span class="text-xs">{description}. Default: None</span>
+                </div>
+            {/if}
+        {/each}
+    </div>
+{/if}
 
 <style>
     .hyperparameters__div {
