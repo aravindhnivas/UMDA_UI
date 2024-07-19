13b2cd1a7bc00e11ac8b04fa7b05d88477bf0a84
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-07-19 19:08:21 -0400

    Update Checkbox.svelte

--- src/lib/components/Checkbox.svelte
+++ src/lib/components/Checkbox.svelte
@@ -2,15 +2,25 @@
     export let value: boolean = false;
     export let label: string = '';
     export let bordered = true;
+    export let check: 'toggle' | 'checkbox' = 'toggle';
+    export let text_position: 'left' | 'right' = 'left';
     let className = '';
     export { className as class };
-    if (bordered) className += 'border-solid border-black border-1 rounded-md';
+    if (check === 'checkbox') {
+        text_position = 'right';
+        bordered = false;
+    }
 </script>
 
-<div class="comp__div {className} form-control w-max">
+<div class="comp__div form-control w-max {bordered && 'border-solid border-black border-1 rounded-md'} {className}">
     <label class="label cursor-pointer gap-2 flex">
-        <span class="label-text">{label}</span>
-        <input type="checkbox" class="toggle" bind:checked={value} />
+        {#if text_position === 'left'}
+            <span class="label-text">{label}</span>
+        {/if}
+        <input type="checkbox" class={check} bind:checked={value} />
+        {#if text_position === 'right'}
+            <span class="label-text">{label}</span>
+        {/if}
     </label>
 </div>
 
@@ -20,5 +30,9 @@
         label {
             padding: 0 0.2rem;
         }
+    }
+
+    input.checkbox {
+        border: solid 1px #000;
     }
 </style>
