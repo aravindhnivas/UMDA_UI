43815772d2af0a2c54f2d352cc3ba075725867d6
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-08-06 12:13:30 -0400

    Update MlPredictions.svelte

--- src/pages/training/predictions/MlPredictions.svelte
+++ src/pages/training/predictions/MlPredictions.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import BrowseFile from '$lib/components/BrowseFile.svelte';
-    import CustomInput from '$lib/components/CustomInput.svelte';
     import Loadingbtn from '$lib/components/Loadingbtn.svelte';
     import Molecule from '$lib/components/Molecule.svelte';
 
@@ -9,7 +8,7 @@
 
     const predict = async () => {
         console.log('predict');
-        predicted_value = '0.5';
+        predicted_value = Math.random().toFixed(2);
     };
 
     let pretrained_model = '';
@@ -17,7 +16,7 @@
         'ml_prediction_molecular_smiles',
         'COP(=S)(OC)OC1=CC=C(C=C1)SC2=CC=C(C=C2)OP(=S)(OC)OC',
     );
-    let predicted_value = '';
+    let predicted_value: number | string = '';
 
     const width = localWritable('ml_prediction_molecular_svg_width', 500);
     const height = localWritable('ml_prediction_molecular_svg_height', 400);
@@ -45,6 +44,11 @@
 
     <div class="flex items-start gap-1">
         <Molecule bind:smiles={$smiles} bind:width={$width} bind:height={$height} />
-        <CustomInput label="Predicted value" value={predicted_value} />
+        <div class="grid gap-2">
+            <div class="text-sm">Predicted value</div>
+            <div class="rounded-1 p-1" style="background-color: antiquewhite;">
+                <span class="select-text">{predicted_value}</span>
+            </div>
+        </div>
     </div>
 </div>
