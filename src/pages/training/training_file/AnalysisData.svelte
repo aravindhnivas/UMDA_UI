4b91c84cfbfdea65c9b57e55451f9b629d8eb91e
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-08-16 15:04:24 -0400

    Update AnalysisData.svelte

--- src/pages/training/training_file/AnalysisData.svelte
+++ src/pages/training/training_file/AnalysisData.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import BrowseFile from '$lib/components/BrowseFile.svelte';
+    import CustomInput from '$lib/components/CustomInput.svelte';
     import Loadingbtn from '$lib/components/Loadingbtn.svelte';
     import { use_dask } from '$lib/stores/system';
     import { training_column_name_X, training_file } from './stores';
@@ -12,6 +14,8 @@
                 key: $training_file.key,
                 use_dask: $use_dask,
                 smiles_column_name: $training_column_name_X,
+                analysis_file: lock_analysis_file ? null : $analysis_file,
+                atoms_bin_size: $atoms_bin_size,
             },
         };
     };
@@ -24,6 +28,9 @@
     };
 
     let loading = false;
+    const analysis_file = localWritable('molecular_analysis_file', '');
+    let lock_analysis_file = false;
+    const atoms_bin_size = localWritable('atoms_bin_size', 10);
 </script>
 
 {#if $training_column_name_X.toLocaleLowerCase() !== 'smiles'}
@@ -36,6 +43,18 @@
         Using {$training_column_name_X} column for molecular structure in file: {$training_file.filename}
     </div>
 {/if}
+
+<BrowseFile
+    bind:filename={$analysis_file}
+    bind:lock={lock_analysis_file}
+    btn_name={'Browse analysis file'}
+    label="Optional. Choose analysis file if you have one!"
+    filters={[{ name: 'molecule_analysis_results', extensions: ['csv'] }]}
+/>
+
+<div class="flex">
+    <CustomInput label="Atoms bin size" bind:value={$atoms_bin_size} type="number" placeholder="Enter atoms bin size" />
+</div>
 
 <Loadingbtn
     class="m-auto"
