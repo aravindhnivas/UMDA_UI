fc2d00467731273ae5fbc04c076f70a22c96b923
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-08-16 20:42:50 -0400

    Update PlotAnalysis.svelte

--- src/pages/training/training_file/plot-analysis/PlotAnalysis.svelte
+++ src/pages/training/training_file/plot-analysis/PlotAnalysis.svelte
@@ -7,9 +7,10 @@
 
     const tab_items = ['size_distribution', 'structural_distribution', 'elemental_distribution'] as const;
     let active_tab: (typeof tab_items)[number] = 'size_distribution';
-    let data: {
+
+    let plotData: {
         size_distribution: Partial<Plotly.PlotData>[];
-        structural_distribution: Partial<Plotly.PlotData>[];
+        structural_distribution: Partial<Plotly.PieData>[];
         elemental_distribution: Partial<Plotly.PlotData>[];
     } = {
         size_distribution: [],
@@ -17,10 +18,67 @@
         elemental_distribution: [],
     };
 
+    const parse_csv_file = async (csv_file: string) => {
+        const contents = await fs.readTextFile(csv_file);
+        const lines = contents.split('\n');
+        const columns = lines[0].split(',');
+        const data = lines.slice(1).map(line => line.split(','));
+        // .filter(line => line.length === columns.length);
+        return { columns, data };
+    };
+
+    const layout: Record<(typeof tab_items)[number], Partial<Plotly.Layout>> = {
+        size_distribution: {
+            xaxis: { title: 'No. of atoms' },
+            yaxis: { title: 'Count', type: 'log' },
+            margin: { t: 0 },
+        },
+        structural_distribution: {},
+        elemental_distribution: {
+            xaxis: { title: 'Element' },
+            yaxis: { title: 'Count', type: 'log' },
+            margin: { t: 0 },
+        },
+    };
+
+    const plot_type: Record<(typeof tab_items)[number], 'bar' | 'pie'> = {
+        size_distribution: 'bar',
+        structural_distribution: 'pie',
+        elemental_distribution: 'bar',
+    };
+
     const onPlot = async (name: (typeof tab_items)[number]) => {
         console.log(name);
 
         const csv_file = $post_analysis_files[name];
+        if (!(await fs.exists(csv_file))) {
+            toast.error(`File ${csv_file} does not exist`);
+            return;
+        }
+
+        const { columns, data } = await parse_csv_file(csv_file);
+        if (plot_type[name] !== 'pie') {
+            plotData[name] = [
+                {
+                    x: data.map(row => row[0]),
+                    y: data.map(row => row[1]),
+                    type: plot_type[name],
+                },
+            ];
+        } else {
+            plotData[name] = [
+                {
+                    labels: data.map(row => row[0]),
+                    values: data.map(row => row[1]),
+                    type: 'pie',
+                    textinfo: 'label+percent',
+                    textposition: 'outside',
+                    automargin: true,
+                    // insidetextorientation: 'radial',
+                },
+            ];
+        }
+        console.log({ columns, data, plotData });
     };
 </script>
 
@@ -46,16 +104,7 @@
             <button class="btn btn-sm" on:click={() => onPlot(name)}>Plot</button>
         </div>
         <div class="h-lg min-w-xl">
-            <Plot
-                data={data[name]}
-                layout={{
-                    xaxis: { title: 'True value' },
-                    yaxis: { title: 'Predicted value' },
-                    margin: { t: 0 },
-                }}
-                fillParent={true}
-                debounce={250}
-            />
+            <Plot data={plotData[name]} layout={layout[name]} fillParent={true} debounce={250} />
         </div>
     </div>
 {/each}
