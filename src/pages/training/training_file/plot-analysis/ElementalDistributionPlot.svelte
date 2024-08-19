4cafbd2ba4f486f756933b1b05b4697c1733735a
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-08-18 21:27:57 -0400

    Update ElementalDistributionPlot.svelte

--- src/pages/training/training_file/plot-analysis/ElementalDistributionPlot.svelte
+++ src/pages/training/training_file/plot-analysis/ElementalDistributionPlot.svelte
@@ -23,20 +23,20 @@
 </script>
 
 <BaseLayout {name} hidden={$active_tab !== name} on:plot={on_plot}>
-    <!-- {#if plotted} -->
-    <h3>Filtering</h3>
-    <button
-        class="btn btn-sm w-max"
-        on:click={() => {
-            selected = select_all ? [...choices] : [];
-            select_all = !select_all;
-        }}>Select {select_all ? 'all' : 'none'}</button
-    >
-    <Set chips={choices} let:chip filter bind:selected>
-        <Chip {chip} touch>
-            <Text>{chip}</Text>
-        </Chip>
-    </Set>
-    <pre class="status">Selected: {selected.length} elements</pre>
-    <!-- {/if} -->
+    {#if plotted}
+        <h3>Filtering</h3>
+        <button
+            class="btn btn-sm w-max"
+            on:click={() => {
+                selected = select_all ? [...choices] : [];
+                select_all = !select_all;
+            }}>Select {select_all ? 'all' : 'none'}</button
+        >
+        <Set chips={choices} let:chip filter bind:selected>
+            <Chip {chip} touch>
+                <Text>{chip}</Text>
+            </Chip>
+        </Set>
+        <pre class="status">Selected: {selected.length} elements</pre>
+    {/if}
 </BaseLayout>
