b07c825ff64da08c3a9c75a397f7f0a5655ccf10
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-08-19 11:23:03 -0400

    Update computefromServer.ts

--- src/lib/pyserver/computefromServer.ts
+++ src/lib/pyserver/computefromServer.ts
@@ -1,7 +1,6 @@
 import { pyServerURL, get } from './stores';
-import { path, fs } from '@tauri-apps/api';
-import { get_tmpdir } from '$utils/index';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+
 interface Type {
     pyfile: string;
     args: Object;
@@ -9,8 +8,6 @@ interface Type {
     general?: boolean;
     cancelToken?: any;
 }
-
-const loading_class = 'running';
 
 export default async function <T>({
     pyfile,
@@ -20,21 +17,7 @@ export default async function <T>({
     cancelToken,
 }: Type): Promise<T | string | undefined> {
     try {
-        console.time('Computation took');
         console.warn({ pyfile, args, target, general });
-        if (!general) {
-            target?.classList.add(loading_class);
-            const filename = pyfile.split('.').at(-1) + '_data.json';
-
-            const tempdirPath = await get_tmpdir();
-            const outputFile = await path.join(tempdirPath, filename);
-            console.warn(await path.dirname(outputFile));
-            if (await fs.exists(outputFile)) {
-                const [_err] = await oO(fs.removeFile(outputFile));
-                if (_err) console.error(_err);
-            }
-        }
-
         const response = await axios.post(
             get(pyServerURL),
             { pyfile, args: { ...args, general } },
@@ -45,17 +28,9 @@ export default async function <T>({
                 cancelToken,
             },
         );
-        // console.log(source);
-        // source.cancel('Operation canceled by the user.');
 
         console.log({ response });
-        if (target?.classList.contains(loading_class)) {
-            target.classList.remove(loading_class);
-        }
-
-        console.timeEnd('Computation took');
         const { data: dataFromPython } = response;
-
         console.warn({ response, dataFromPython });
         if (response.statusText !== 'OK') {
             return Promise.reject(dataFromPython);
@@ -71,15 +46,16 @@ export default async function <T>({
         }
 
         return Promise.resolve(dataFromPython);
-    } catch (error) {
-        if (target?.classList.contains(loading_class)) {
-            target.classList.remove(loading_class);
-        }
-
-        if (error instanceof Error) {
-            const msg = error.message;
-            const details = error.stack || error;
-            // console.error(error, error.response?.data);
+    } catch (err) {
+        if (axios.isAxiosError(err)) {
+            if (!err.response) return Promise.reject('Internal error: no response from server');
+            console.error(err.response.data);
+            const { error, traceback } = err.response.data as { error: string; traceback: string };
+            return Promise.reject(`${error}\n${traceback}`);
+        } else if (err instanceof Error) {
+            console.error('Non-Axios error:', err);
+            const msg = err.message;
+            const details = err.stack || err;
             return Promise.reject(new Error(`Error after receiving data from python \n${msg} \n${details}`));
         }
     }
