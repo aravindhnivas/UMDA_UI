0916b1bd3a6b45791f71bfd0517d3cea2917cc35
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-08-21 17:24:12 -0400

    Update PlotAnalysis.svelte

--- src/pages/training/training_file/plot-analysis/PlotAnalysis.svelte
+++ src/pages/training/training_file/plot-analysis/PlotAnalysis.svelte
@@ -6,11 +6,21 @@
     import Tab, { Label } from '@smui/tab';
     import TabBar from '@smui/tab-bar';
     import { parse_csv_file } from '$lib/utils';
+    import CustomSelect from '$lib/components/CustomSelect.svelte';
+    import { RefreshCcw } from 'lucide-svelte';
+    import { isArray } from 'lodash-es';
 
     const tab_items = ['size_distribution', 'structural_distribution', 'elemental_distribution'] as const;
 
     const GetData = async <T = string | number,>(name: string) => {
-        const csv_file = await path.join(await $post_analysis_files_directory, name);
+        let csv_file: string;
+        const analysis_dir = await $post_analysis_files_directory;
+        if (filtered_dir === 'default') {
+            csv_file = await path.join(analysis_dir, name);
+        } else {
+            csv_file = await path.join(analysis_dir, 'filtered', filtered_dir, name);
+        }
+        // csv_file = await path.join(analysis_dir, name);
         if (!(await fs.exists(csv_file))) {
             toast.error(`File ${csv_file} does not exist`);
             return;
@@ -27,9 +37,31 @@
         return { x, y };
     };
     setContext('GetData', GetData);
+    let filtered_dir = 'default';
+    let dir_items_for_plotting: string[] = ['default'];
+
+    const fetch_analysis_dir = async () => {
+        const analysis_dir = await $post_analysis_files_directory;
+        const filtered_dir = await path.join(analysis_dir, 'filtered');
+        const dirs = await fs.readDir(filtered_dir);
+        if (isEmpty(dirs)) {
+            toast.error(`No directories found in ${filtered_dir}`);
+            return;
+        }
+        const dir_items = dirs.filter(dir => isArray(dir.children)).map(dir => dir.name as string);
+        dir_items_for_plotting = ['default', ...dir_items.filter(Boolean)];
+        console.log(dir_items);
+    };
 </script>
 
 <h3>Analysis plots</h3>
+
+<div class="flex gap-2 items-end">
+    <button class="btn btn-sm" on:click={fetch_analysis_dir}>
+        <RefreshCcw />
+    </button>
+    <CustomSelect label="Select analysis directory" bind:value={filtered_dir} items={dir_items_for_plotting} />
+</div>
 
 <div class="w-max">
     <TabBar tabs={[...tab_items]} let:tab bind:active={$active_tab}>
