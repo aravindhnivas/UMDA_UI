2f769ce8a422496660333a93903ed73e9717927e
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-08-24 09:51:58 -0400

    virtual list

--- src/pages/training/embedding/Results.svelte
+++ src/pages/training/embedding/Results.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { CheckCheck } from 'lucide-svelte/icons';
     import { training_column_name_X } from '../training_file/stores';
+    import VirtualList from '@sveltejs/svelte-virtual-list';
 
     export let computed_time: string;
     export let data: {
@@ -13,6 +14,7 @@
 
 {#if data}
     {@const { invalid_smiles, saved_file } = data}
+    {@const invalid_smiles_indexed = invalid_smiles.map((smiles, ind) => ({ smiles, ind }))}
     <button class="btn btn-sm btn-error w-max ml-auto" on:click={() => (data = null)}>X</button>
     <div class=" flex flex-col gap-1">
         {#if saved_file}
@@ -27,20 +29,9 @@
                 Could not compute embeddings for {invalid_smiles.length}
                 {$training_column_name_X}
             </h3>
-            <ul class="invalid_smi_list px-4 select-text">
-                {#each invalid_smiles as smiles, ind}
-                    <li>{ind + 1}: <span class="select-text text-wrap break-all">{smiles}</span></li>
-                {/each}
-            </ul>
+            <VirtualList height="300px" items={invalid_smiles_indexed} let:item>
+                <span class="select-text text-wrap break-all">{item.ind + 1}: {item.smiles}</span>
+            </VirtualList>
         {/if}
     </div>
 {/if}
-
-<style>
-    .invalid_smi_list {
-        list-style-type: none;
-        max-height: 300px;
-        overflow: auto;
-        max-width: calc(70vw - 1rem);
-    }
-</style>
