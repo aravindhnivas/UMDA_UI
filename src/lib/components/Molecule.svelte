335b113083a34056b2268d5d50b1416e0b8d0060
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-08-30 15:18:18 -0400

    Update Molecule.svelte

--- src/lib/components/Molecule.svelte
+++ src/lib/components/Molecule.svelte
@@ -25,11 +25,11 @@
         <div class="grid grid-cols-5 gap-2">
             <div class="col-span-2 grid gap-1">
                 <span class="text-sm">Width: {width}px</span>
-                <input type="range" bind:value={width} min="100" max="1000" step="10" class="range" />
+                <input class="range range-xs" type="range" bind:value={width} min="100" max="1000" step="10" />
             </div>
             <div class="grid gap-1 col-span-2">
                 <span class="text-sm">Height: {height}px</span>
-                <input type="range" bind:value={height} min="100" max="1000" step="10" class="range" />
+                <input class="range range-xs" type="range" bind:value={height} min="100" max="1000" step="10" />
             </div>
             <button class="btn btn-sm btn-outline" on:click={() => ((width = 700), (height = 400))}>Restore</button>
         </div>
