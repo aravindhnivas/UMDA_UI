71b0551d146f18320f04756fb60a68b555b731b7
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-07 18:58:17 -0400

    Update TerminalBox.svelte

--- src/lib/components/TerminalBox.svelte
+++ src/lib/components/TerminalBox.svelte
@@ -5,10 +5,9 @@
     export let height = '400px';
     let className = '';
     export { className as class };
-
+    let mounted = false;
     onMount(() => {
-        console.log('TerminalBox mounted');
-        terminal = [{ type: 'info', message: 'Terminal mounted' }];
+        mounted = true;
     });
 </script>
 
@@ -21,6 +20,9 @@
     }}>Clear</button
 >
 <div class="mockup-code {className}" style:height>
+    {#if mounted && terminal.length === 0}
+        <pre data-prefix=">" class="text-info"><code class="select-text">Terminal Initialized</code></pre>
+    {/if}
     {#if terminal.length > 0}
         <VirtualList height="90%" items={terminal} let:item>
             <pre data-prefix={item.prefix || '>'} class="text-{item.type}"><code
