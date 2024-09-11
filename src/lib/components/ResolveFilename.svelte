fbbdbe17dffc7a4a4c10f9ca9c887a9ffd460ba0
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-11 11:47:46 -0400

    Update ResolveFilename.svelte

--- src/lib/components/ResolveFilename.svelte
+++ src/lib/components/ResolveFilename.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
     export let filename: Promise<string> | string;
     export let basename: boolean = true;
-    export let status: 'success' | 'info' | 'primary' | 'warning' = 'success';
     let className = '';
     export { className as class };
-
+    export let absolute = false;
     const get_basename_or_dirname = async (filename: string) => {
         if (basename) return path.basename(await filename);
         return path.dirname(await filename);
@@ -14,11 +13,17 @@
 {#if filename}
     {#await filename then file_path}
         {#await fs.exists(file_path) then file_exists}
-            {#await get_basename_or_dirname(file_path) then fname}
-                <div class="badge badge-{status} {className}" class:badge-error={!file_exists}>
-                    {fname || (basename ? 'file' : 'directory')} exists
+            {#if absolute}
+                <div class={className} class:badge-error={!file_exists}>
+                    {file_path || (basename ? 'file' : 'directory') + ' exists'}
                 </div>
-            {/await}
+            {:else}
+                {#await get_basename_or_dirname(file_path) then fname}
+                    <div class={className} class:badge-error={!file_exists}>
+                        {fname || (basename ? 'file' : 'directory') + ' exists'}
+                    </div>
+                {/await}
+            {/if}
         {/await}
     {/await}
 {:else}
