d4fbf8852f32210287ed0a60cf84c0882376e90a
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-11 19:41:55 -0400

    Update TrainingFilePanel.svelte

--- src/pages/training/MLmodel/TrainingFilePanel.svelte
+++ src/pages/training/MLmodel/TrainingFilePanel.svelte
@@ -4,28 +4,34 @@
     import { training_column_name_y } from '../training_file/stores';
     import { current_training_data_file } from '../training_file/plot-analysis/stores';
     import { RefreshCcw } from 'lucide-svelte';
-    import ResolveFilename from '$lib/components/ResolveFilename.svelte';
 
     let refresh = true;
 </script>
 
 <CustomPanel title="Loaded training file" open={true}>
-    <button class="flex btn btn-sm ml-auto" on:click={() => (refresh = !refresh)}>
+    <button class="flex btn btn-sm ml-auto mb-2" on:click={() => (refresh = !refresh)}>
         <span>Refresh</span>
         <RefreshCcw size="20" />
     </button>
 
     {#key refresh}
         <div class="grid gap-2 grid-cols-4 items-center">
-            <!-- <ResolveFilename
-                filename={$current_training_data_file}
-                basename={false}
-                class="badge badge-success col-span-4 h-[2.5rem] flex m-auto"
-            /> -->
             <div>Training file:</div>
-            <ResolveFilename filename={$current_training_data_file} class="badge badge-success col-span-3" />
+            {#await $current_training_data_file then file_name}
+                {#await fs.exists(file_name) then file_exists}
+                    <div class="col-span-3 bg-success border-rounded p-1" class:bg-red={!file_exists}>
+                        <code class=" break-words text-sm">{file_name}</code>
+                    </div>
+                {/await}
+            {/await}
             <div>Embedded vector file:</div>
-            <ResolveFilename filename={$embedd_savefile_path} class="badge badge-success col-span-3" />
+            {#await $embedd_savefile_path then file_name}
+                {#await fs.exists(file_name) then file_exists}
+                    <div class="col-span-3 bg-success border-rounded p-1" class:bg-red={!file_exists}>
+                        <code class=" break-words text-sm">{file_name}</code>
+                    </div>
+                {/await}
+            {/await}
             <div>Column (train_y):</div>
             <div class="badge badge-success col-span-3" class:badge-error={!$training_column_name_y}>
                 {$training_column_name_y || 'Column not provided'}
