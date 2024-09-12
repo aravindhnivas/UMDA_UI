6247684b790fd46ed0f146c21a2c2a1a28eb0640
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-12 12:19:18 -0400

    Update TrainingFilePanel.svelte

--- src/pages/training/MLmodel/TrainingFilePanel.svelte
+++ src/pages/training/MLmodel/TrainingFilePanel.svelte
@@ -1,19 +1,8 @@
 <script lang="ts">
     import CustomPanel from '$lib/components/CustomPanel.svelte';
-    import { training_column_name_y } from '../training_file/stores';
-    import { RefreshCcw } from 'lucide-svelte';
     import LoadedFileInfos from '../embedding/LoadedFileInfos.svelte';
-
-    let refresh = true;
 </script>
 
 <CustomPanel title="Loaded training file" open={true}>
-    <button class="flex btn btn-sm ml-auto mb-2" on:click={() => (refresh = !refresh)}>
-        <span>Refresh</span>
-        <RefreshCcw size="20" />
-    </button>
-
-    {#key refresh}
-        <LoadedFileInfos />
-    {/key}
+    <LoadedFileInfos />
 </CustomPanel>
