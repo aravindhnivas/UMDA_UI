2525d5e4392547cb84dfa5be7e51fbf5e002a80f
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-17 13:59:47 -0400

    Update LoadedFileInfos.svelte

--- src/pages/training/embedding/LoadedFileInfos.svelte
+++ src/pages/training/embedding/LoadedFileInfos.svelte
@@ -1,22 +1,12 @@
 <script lang="ts">
-    import { training_file, training_state_loaded } from '../training_file/stores';
     import { current_training_data_file } from '../training_file/plot-analysis/stores';
     import { embedd_savefile_path } from './stores';
     import { training_column_name_X, training_column_name_y } from '../training_file/stores';
-    import { RefreshCcw, ArrowBigRight } from 'lucide-svelte/icons';
 
-    export let show_status = true;
-    export let show_embedded_file = true;
+    // export let show_status = true;
+    // export let show_embedded_file = true;
 
     const dispatch = createEventDispatcher();
-    // let refresh = true;
-
-    let loaded_files = {
-        training_file: '',
-        embedded_file: '',
-        columnX: '',
-        columnY: '',
-    } as Record<string, string>;
 
     let items = [
         { name: 'Training file', key: 'training_file' },
@@ -25,58 +15,44 @@
         { name: 'Train y', key: 'columnY' },
     ];
 
-    const refresh_data = async () => {
-        await tick();
-        let file_name: string;
+    const refresh_data = async (
+        tfile: Promise<string>,
+        vfile: Promise<string>,
+        columnX: string,
+        columnY: string,
+    ): Promise<Record<string, string>> => {
+        let loaded_files: Record<string, string> = {
+            training_file: '',
+            embedded_file: '',
+            columnX: '',
+            columnY: '',
+        };
+        const [_training_file, _embedded_file] = await Promise.all([tfile, vfile]);
 
-        file_name = await $current_training_data_file;
-        if (await fs.exists(file_name)) {
-            loaded_files.training_file = file_name;
-        } else {
-            loaded_files.training_file = '';
+        if (await fs.exists(_training_file)) {
+            loaded_files.training_file = _training_file;
         }
 
-        file_name = await $embedd_savefile_path;
-        if (await fs.exists(file_name)) {
-            loaded_files.embedded_file = file_name;
-        } else {
-            loaded_files.embedded_file = '';
+        if (await fs.exists(_embedded_file)) {
+            loaded_files.embedded_file = _embedded_file;
         }
 
-        loaded_files.columnX = $training_column_name_X || '';
-        loaded_files.columnY = $training_column_name_y || '';
-        items = [...items]; // force update
+        loaded_files.columnX = columnX || '';
+        loaded_files.columnY = columnY || '';
         console.log('Loaded files:', loaded_files);
         dispatch('refresh', loaded_files);
+        return loaded_files;
     };
-
-    onMount(async () => {
-        await refresh_data();
-    });
-    $: if ($training_file.filename || $training_state_loaded) refresh_data();
 </script>
 
-<div class="flex items-center justify-between">
-    {#if show_status}
-        <div class="flex gap-1 items-end">
-            <code>File status indicator</code>
-            <ArrowBigRight size="20" />
-            <code class="badge badge-success">EXISTS</code>
-            <code class="badge badge-error">DOESN'T-EXISTS</code>
-        </div>
-    {/if}
-    <button class="flex btn btn-sm mb-2" on:click={refresh_data}>
-        <span>Refresh</span>
-        <RefreshCcw size="20" />
-    </button>
-</div>
-
-<div class="grid gap-2 grid-cols-4 items-center">
-    {#each items as { name, key }}
-        {@const val = loaded_files[key]}
-        <div>{name}:</div>
-        <div class="col-span-3 border-rounded" class:bg-red={!val}>
-            <code class=" break-all text-sm p-1" class:bg-success={val}>{val || `Invalid ${key} name`}</code>
-        </div>
-    {/each}
-</div>
+{#await refresh_data($current_training_data_file, $embedd_savefile_path, $training_column_name_X, $training_column_name_y) then loaded_files}
+    <div class="grid gap-2 grid-cols-4 items-center">
+        {#each items as { name, key }}
+            {@const val = loaded_files[key]}
+            <div>{name}:</div>
+            <div class="col-span-3 border-rounded" class:bg-red={!val}>
+                <code class=" break-all text-sm p-1" class:bg-success={val}>{val || `Invalid ${key} name`}</code>
+            </div>
+        {/each}
+    </div>
+{/await}
