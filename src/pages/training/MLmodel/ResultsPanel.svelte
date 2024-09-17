fec2bed072e877d9d30cc41aba6201716a7ffef9
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-17 11:13:40 -0400

    Update ResultsPanel.svelte

--- src/pages/training/MLmodel/ResultsPanel.svelte
+++ src/pages/training/MLmodel/ResultsPanel.svelte
@@ -21,35 +21,28 @@
         };
     }
 
-    let test_data: Record<MLModel, ParsedData['test'] | null> = model_names.reduce<
-        Record<MLModel, ParsedData['test'] | null>
-    >(
-        (acc, model) => {
-            acc[model] = null;
-            return acc;
-        },
-        {} as Record<MLModel, ParsedData['test'] | null>,
-    );
-
-    let train_data: Record<MLModel, ParsedData['train'] | null> = model_names.reduce<
-        Record<MLModel, ParsedData['train'] | null>
-    >(
-        (acc, model) => {
-            acc[model] = null;
-            return acc;
-        },
-        {} as Record<MLModel, ParsedData['train'] | null>,
-    );
+    let test_data = {} as Record<MLModel, ParsedData['test']>;
+    let train_data = {} as Record<MLModel, ParsedData['train']>;
 
     const on_plot_data_ready = async () => {
-        test_data[$model] = null;
-        train_data[$model] = null;
-
         try {
+            const filename = await path.basename(data_file);
+            const file_model = filename.split('_')[0] as MLModel;
+            $model = file_model;
+            console.log('Reading plot data\n', filename, file_model, $model);
+
             const saved_file_contents = await fs.readTextFile(data_file);
             const parsed = JSON.parse(saved_file_contents) as ParsedData;
+            console.warn({ parsed });
+
             test_data[$model] = parsed.test;
             train_data[$model] = parsed.train;
+
+            if (!(test_data[$model] && train_data[$model])) {
+                toast.error('Error reading plot data');
+                return;
+            }
+
             $plot_data[$model] = [
                 {
                     x: test_data[$model].y_true,
@@ -70,13 +63,13 @@
             ];
             if (!$include_training_file_in_plot) return;
             $plot_data[$model] = [
-                ...$plot_data[$model],
                 {
                     x: train_data[$model].y_true,
                     y: train_data[$model].y_pred,
                     mode: 'markers',
                     type: 'scatter',
                     name: 'TRAIN: Predicted',
+                    opacity: 0.5,
                     marker: { color: PlotlyColors.safety_orange },
                 },
                 {
@@ -87,6 +80,7 @@
                     name: 'TRAIN: Linear fit',
                     line: { color: PlotlyColors.safety_orange },
                 },
+                ...$plot_data[$model],
             ];
         } catch (error) {
             toast.error('Error reading plot data\n' + error);
@@ -98,6 +92,7 @@
     }
 
     const include_training_plot_if_required = async () => {
+        console.warn($include_training_file_in_plot);
         if (!$plot_data[$model]?.length) return;
         if (!(train_data[$model] && test_data[$model])) return;
 
@@ -108,13 +103,13 @@
         }
         if (!$include_training_file_in_plot) return;
         $plot_data[$model] = [
-            ...$plot_data[$model],
             {
                 x: train_data[$model].y_true,
                 y: train_data[$model].y_pred,
                 mode: 'markers',
                 type: 'scatter',
                 name: 'TRAIN: Predicted',
+                opacity: 0.5,
                 marker: { color: PlotlyColors.safety_orange },
             },
             {
@@ -125,6 +120,7 @@
                 name: 'TRAIN: Linear fit',
                 line: { color: PlotlyColors.safety_orange },
             },
+            ...$plot_data[$model],
         ];
     };
 </script>
@@ -139,54 +135,58 @@
     </div>
 
     <div class="grid gap-2">
-        {#if $results[$model]}
+        {#if $results?.[$model]}
             {@const r = $results[$model]}
-            {@const train_stats = r.train_stats}
-            {@const test_stats = r.test_stats}
+            {#if r}
+                {@const train_stats = r.train_stats}
+                {@const test_stats = r.test_stats}
 
-            <div class="flex gap-1">
-                <span class="badge badge-primary">
-                    {r.time ? `completed in ${$results[$model].time}` : ''}
-                </span>
-                <span class="badge badge-primary">Dataset size: {r.data_shapes.y}</span>
-                <!-- <span class="badge badge-info"># y_test_data_size ({$test_size}%): {r.data_shapes.y_test}</span> -->
-            </div>
-            <div class="grid grid-cols-6 gap-2 items-center w-3xl">
-                <span class="badge badge-info col-span-2"
-                    >Train stats ({100 - $test_size}% data = {r.data_shapes.y_train}):
-                </span>
-                <span class="badge">R<sup>2</sup>: {train_stats.r2}</span>
-                <span class="badge">MSE: {train_stats.mse}</span>
-                <span class="badge">RMSE: {train_stats.rmse}</span>
-                <span class="badge">MAE: {train_stats.mae}</span>
-            </div>
-            <div class="grid grid-cols-6 gap-2 items-center w-3xl">
-                <span class="badge badge-warning col-span-2"
-                    >Test stats ({$test_size}% data = {r.data_shapes.y_test}):
-                </span>
-                <span class="badge">R<sup>2</sup>: {test_stats.r2}</span>
-
-                <span class="badge">MSE: {test_stats.mse}</span>
-                <span class="badge">RMSE: {test_stats.rmse}</span>
-                <span class="badge">MAE: {test_stats.mae}</span>
-            </div>
-
-            {#if r.cross_validation && r.cv_scores}
-                <span class="badge">{r.cv_fold}-fold CV - R<sup>2</sup>: {r.cv_scores.mean} ({r.cv_scores.std})</span>
-            {/if}
-
-            {#if r.best_params}
-                <hr />
-                <div class="grid gap-2">
-                    <h3>Best parameters</h3>
-                    <span class="text-sm">Best score: {r.best_score}</span>
-                    <div class="flex gap-2 flex-wrap">
-                        {#each Object.entries(r.best_params) as [key, value]}
-                            <span class="badge badge-info">{key}: {value}</span>
-                        {/each}
-                    </div>
+                <div class="flex gap-1">
+                    <span class="badge badge-primary">
+                        {r.time ? `completed in ${$results[$model].time}` : ''}
+                    </span>
+                    <span class="badge badge-primary">Dataset size: {r.data_shapes.y}</span>
+                    <!-- <span class="badge badge-info"># y_test_data_size ({$test_size}%): {r.data_shapes.y_test}</span> -->
                 </div>
-                <hr />
+                <div class="grid grid-cols-6 gap-2 items-center w-3xl">
+                    <span class="badge badge-warning col-span-2"
+                        >Train stats ({100 - $test_size}% data = {r.data_shapes.y_train}):
+                    </span>
+                    <span class="badge">R<sup>2</sup>: {train_stats.r2}</span>
+                    <span class="badge">MSE: {train_stats.mse}</span>
+                    <span class="badge">RMSE: {train_stats.rmse}</span>
+                    <span class="badge">MAE: {train_stats.mae}</span>
+                </div>
+                <div class="grid grid-cols-6 gap-2 items-center w-3xl">
+                    <span class="badge badge-info col-span-2"
+                        >Test stats ({$test_size}% data = {r.data_shapes.y_test}):
+                    </span>
+                    <span class="badge">R<sup>2</sup>: {test_stats.r2}</span>
+
+                    <span class="badge">MSE: {test_stats.mse}</span>
+                    <span class="badge">RMSE: {test_stats.rmse}</span>
+                    <span class="badge">MAE: {test_stats.mae}</span>
+                </div>
+
+                {#if r.cross_validation && r.cv_scores}
+                    <span class="badge"
+                        >{r.cv_fold}-fold CV - R<sup>2</sup>: {r.cv_scores.mean} ({r.cv_scores.std})</span
+                    >
+                {/if}
+
+                {#if r.best_params}
+                    <hr />
+                    <div class="grid gap-2">
+                        <h3>Best parameters</h3>
+                        <span class="text-sm">Best score: {r.best_score}</span>
+                        <div class="flex gap-2 flex-wrap">
+                            {#each Object.entries(r.best_params) as [key, value]}
+                                <span class="badge badge-info">{key}: {value}</span>
+                            {/each}
+                        </div>
+                    </div>
+                    <hr />
+                {/if}
             {/if}
         {/if}
 
