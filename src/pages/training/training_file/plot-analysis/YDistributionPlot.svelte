f8f724874621c5ca5a648f941a36c576d10cb1f7
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-17 22:28:21 -0400

    Update YDistributionPlot.svelte

--- src/pages/training/training_file/plot-analysis/YDistributionPlot.svelte
+++ src/pages/training/training_file/plot-analysis/YDistributionPlot.svelte
@@ -6,6 +6,7 @@
     import { training_column_name_y, training_file, training_save_directory } from '../stores';
     import { active_tab } from './stores';
     import Plot from 'svelte-plotly.js';
+    import Checkbox from '$lib/components/Checkbox.svelte';
 
     let data: Partial<Plotly.PlotData>[] = [];
 
@@ -20,18 +21,15 @@
                 column_name: $training_column_name_y,
                 training_save_directory: $training_save_directory,
                 bin_size,
+                auto_bin_size,
             },
         };
     };
 
     const layout = {
         title: `${$training_column_name_y} Distribution`,
-        xaxis: {
-            title: 'Bin Edges',
-        },
-        yaxis: {
-            title: 'Counts',
-        },
+        xaxis: { title: 'Value' },
+        yaxis: { title: 'Frequency' },
     };
 
     const onResult = async (e: CustomEvent) => {
@@ -42,10 +40,14 @@
         const filename = dataFromPython.filename;
         if (!filename) return;
 
+        if (auto_bin_size) {
+            bin_size = dataFromPython.bin_size;
+        }
         const contents = await fs.readTextFile(filename);
-        const { hist, bin_edges } = JSON.parse(contents);
+        const parsed_data = JSON.parse(contents);
+        console.log({ parsed_data });
+        const { hist, bin_edges } = parsed_data;
         // Create x values for Plotly bar chart
-        console.log({ contents, hist, bin_edges });
         let xValues = [];
         for (let i = 0; i < bin_edges.length - 1; i++) {
             xValues.push((bin_edges[i] + bin_edges[i + 1]) / 2);
@@ -64,14 +66,15 @@
             },
         ];
     };
-
+    let auto_bin_size: boolean = true;
     let bin_size: string | number = 30;
 </script>
 
 <div class="grid gap-2" class:hidden={$active_tab !== 'y-data_distribution'}>
     <span class="badge">using {$training_column_name_y} column to analyse distribution</span>
     <div class="flex items-end gap-1">
-        <CustomInput bind:value={bin_size} label="Bin" type="number" />
+        <Checkbox bind:value={auto_bin_size} label="auto bin_size" />
+        <CustomInput bind:value={bin_size} label="bin_size" type="number" />
         <Loadingbtn callback={analysis_y_data_distribution} on:result={onResult} />
     </div>
     <div class="h-lg min-w-xl">
