26fb8ea24fb9c945d4dfa397262b4dc68e602197
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-18 15:37:30 -0400

    Update NormalLoadingBtn.svelte

--- src/lib/components/NormalLoadingBtn.svelte
+++ src/lib/components/NormalLoadingBtn.svelte
@@ -3,16 +3,20 @@
     export let name: string = 'Compute';
 
     let loading = false;
+    const dispatch = createEventDispatcher();
     const load = async () => {
         try {
             loading = true;
             const data = await callback();
             toast.success('Comutation done successfully');
-            return data;
+            dispatch('result', data);
+            // return data;
         } catch (error) {
             console.error(error);
+            dispatch('error', error);
             toast.error(error as string);
         } finally {
+            dispatch('done');
             loading = false;
         }
     };
