89ca48c83d46f0a3b43873074c830d014e6b93d4
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-17 20:16:41 -0400

    Update types.d.ts

--- src/pages/training/training_file/plot-analysis/types.d.ts
+++ src/pages/training/training_file/plot-analysis/types.d.ts
@@ -1,2 +1,6 @@
-type AnalysisItemsType = 'size_distribution' | 'structural_distribution' | 'elemental_distribution';
+type AnalysisItemsType =
+    | 'y-data_distribution'
+    | 'size_distribution'
+    | 'structural_distribution'
+    | 'elemental_distribution';
 type MolecularAnalysisFunction = (mode: string) => Promise<{ pyfile: string; args: Record<string, any> }>;
