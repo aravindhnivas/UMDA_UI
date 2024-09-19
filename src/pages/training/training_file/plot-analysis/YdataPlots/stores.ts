1e126d2f7ad588814ecb39654d6702ec48fa3e9a
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-19 17:54:00 -0400

    Update stores.ts

--- src/pages/training/training_file/plot-analysis/YdataPlots/stores.ts
+++ src/pages/training/training_file/plot-analysis/YdataPlots/stores.ts
@@ -1,8 +1,18 @@
+import { current_post_analysis_files_directory } from '../stores';
+
 export const ytransformation = writable<string>('None');
 const y_data_distribution_savefilename = 'y_data_distribution';
 export const savefilename = derived(ytransformation, $ytransformation => {
     if ($ytransformation == 'None') return `${y_data_distribution_savefilename}.json`;
-    return `${y_data_distribution_savefilename}_${$ytransformation}.json`;
+    return `${y_data_distribution_savefilename}_for_${$ytransformation}_transformation.json`;
 });
 export const min_yvalue = writable<string>('');
 export const max_yvalue = writable<string>('');
+// const save_loc = await path.join(await $current_post_analysis_files_directory, 'y_data_distribution');
+export const save_loc_for_ydistribution_data = derived(
+    current_post_analysis_files_directory,
+    async $current_post_analysis_files_directory => {
+        const analysis_dir = await $current_post_analysis_files_directory;
+        return await path.join(analysis_dir, 'y_data_distribution');
+    },
+);
