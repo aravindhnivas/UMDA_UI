692e696e51e57ed6619214c285b5dc6d5588aea7
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-19 15:16:57 -0400

    Update ControlPanel.svelte

--- src/pages/training/MLmodel/ControlPanel.svelte
+++ src/pages/training/MLmodel/ControlPanel.svelte
@@ -24,6 +24,7 @@
     } from './stores';
     import { Checkbox, CustomSelect, CustomInput } from '$lib/components';
     import CustomPanel from '$lib/components/CustomPanel.svelte';
+    import Paper, { Title, Content } from '@smui/paper';
 
     const grid_search_methods = [
         'GridSearchCV',
@@ -33,107 +34,163 @@
         'DaskGridSearchCV',
         'DaskRandomSearchCV',
     ];
+    const elevation = 3;
+    const paper_style = 'background-color: whitesmoke;';
 </script>
 
-<CustomPanel title="Control" open={true}>
-    <div class="flex flex-wrap gap-4">
-        <div class="grid gap-1">
-            <input class="range" type="range" min="0" max="100" step="5" bind:value={$test_size} />
-            <span>split: {$test_size}% test : {100 - $test_size}% train</span>
-            {#if $test_size > 50}
-                <div class="badge badge-sm badge-warning">Warning: Test split ratio is greater than 50%</div>
-            {/if}
-            <div class="grid grid-cols-2 gap-1 border-rounded border-solid border-2 p-1">
-                <div class="grid">
-                    <CustomSelect
-                        items={available_transformations}
-                        label="y-transformation"
-                        bind:value={$ytransformation}
-                    />
-                    {#if $ytransformation !== 'None'}
-                        <Checkbox bind:value={$inverse_transform} label="apply inverse_transform" check="checkbox" />
-                    {/if}
+<CustomPanel open={false}>
+    <svelte:fragment slot="title" let:open>
+        <div class="flex-center">
+            <span>Control Panel</span>
+            {#if !open}
+                <div class="join">
+                    <span class="join-item badge-sm badge-warning">{100 - $test_size}</span>
+                    <span class="join-item badge-sm badge-info">{$test_size}</span>
                 </div>
-                <div class="grid">
-                    <CustomSelect items={available_scalers} label="y-scaling" bind:value={$yscaling} />
-                    {#if $yscaling !== 'None'}
-                        <Checkbox bind:value={$inverse_scaling} label="apply inverse_scaling" check="checkbox" />
-                    {/if}
-                </div>
+                {#if $ytransformation !== 'None'}
+                    <span class="badge-sm badge-info">{$ytransformation}</span>
+                {/if}
+                {#if $yscaling !== 'None'}
+                    <span class="badge-sm badge-info">{$yscaling}</span>
+                {/if}
+                {#if $bootstrap}
+                    <span class="badge-sm badge-warning">Augment</span>
+                {/if}
 
-                {#if !$inverse_scaling || !$inverse_transform}
-                    <span class="text-sm font-400 col-span-2 text-wrap break-all">
+                {#if $fine_tune_model}
+                    <span class="badge-sm badge-info">Fine-tune</span>
+                {/if}
+                {#if $cross_validation}
+                    <span class="badge-sm badge-info">Cross-validation</span>
+                {/if}
+                {#if $parallel_computation}
+                    <span class="badge-sm badge-primary">Parallel</span>
+                {/if}
+                {#if $skip_invalid_y_values}
+                    <span class="badge-sm badge-warning">skip_invalid_y_values</span>
+                {/if}
+            {/if}
+        </div>
+    </svelte:fragment>
+
+    <div class="grid gap-4">
+        <Paper transition {elevation} style={paper_style}>
+            <Title>Data split-ratio, transformation and scaling</Title>
+            <Content class="grid gap-2">
+                <input class="range" type="range" min="0" max="100" step="5" bind:value={$test_size} />
+                <span class="text-sm font-400">Split ratios: {$test_size}% test : {100 - $test_size}% train</span>
+                {#if $test_size > 50}
+                    <div class="badge badge-sm badge-warning">Warning: Test split ratio is greater than 50%</div>
+                {/if}
+                <div class="grid grid-cols-2 gap-1">
+                    <div class="grid">
+                        <CustomSelect
+                            items={available_transformations}
+                            label="y-transformation"
+                            bind:value={$ytransformation}
+                        />
+                        {#if $ytransformation !== 'None'}
+                            <Checkbox
+                                bind:value={$inverse_transform}
+                                label="apply inverse_transform"
+                                check="checkbox"
+                            />
+                        {/if}
+                    </div>
+                    <div class="grid">
+                        <CustomSelect items={available_scalers} label="y-scaling" bind:value={$yscaling} />
+                        {#if $yscaling !== 'None'}
+                            <Checkbox bind:value={$inverse_scaling} label="apply inverse_scaling" check="checkbox" />
+                        {/if}
+                    </div>
+
+                    <!-- {#if !$inverse_scaling || !$inverse_transform} -->
+                    <span class="badge-sm badge-info col-span-2">
                         Always inverse the transformations and scaling applied to your target variable before
                         calculating metrics.
                     </span>
-                {/if}
-            </div>
-        </div>
-        <div class="grid grid-cols-2 gap-1 items-end border-rounded border-solid border-2 p-1">
-            <Checkbox bind:value={$fine_tune_model} label="Grid search" check="checkbox" />
-            <Checkbox bind:value={$cross_validation} label="Cross validation" check="checkbox" />
-            <CustomSelect items={grid_search_methods} bind:value={$grid_search_method} disabled={!$fine_tune_model} />
-            <CustomInput
-                bind:value={$cv_fold}
-                min="2"
-                type="number"
-                disabled={!($cross_validation || $fine_tune_model)}
-            />
-            <CustomInput
-                label="n_iter"
-                bind:value={$randomzied_gridsearch_niter}
-                min="2"
-                type="number"
-                disabled={!$fine_tune_model}
-            />
-            <CustomInput
-                label="Halving factor"
-                bind:value={$halving_factor}
-                min="2"
-                type="number"
-                disabled={!$fine_tune_model}
-            />
-        </div>
-        <div class="grid gap-1 border-rounded border-solid border-2 p-1">
-            <Checkbox bind:value={$bootstrap} label="bootstrap" check="checkbox" />
-            <div class="flex gap-2">
-                <CustomInput
-                    label="n_samples"
-                    bind:value={$bootstrap_nsamples}
-                    type="number"
-                    step="1"
-                    disabled={!$bootstrap}
-                />
-
-                <CustomInput
-                    label="noise (%)"
-                    bind:value={$noise_percentage}
-                    type="number"
-                    step="0.1"
-                    min="0"
-                    max="100"
-                    disabled={!$bootstrap}
-                />
-            </div>
-        </div>
-        <div class="grid gap-1 border-rounded border-solid border-2 p-1">
-            <Checkbox bind:value={$parallel_computation} label="Parallel computation" check="checkbox" />
-            <div class="flex gap-2 items-end">
-                <CustomInput
-                    label="n_jobs"
-                    bind:value={$n_jobs}
-                    type="number"
-                    step="1"
-                    disabled={!$parallel_computation}
-                />
+                    <!-- {/if} -->
+                </div>
+            </Content>
+        </Paper>
+        <Paper transition {elevation} style={paper_style}>
+            <Title>Fine tuning parameters and Cross-validation metrics</Title>
+            <Content class="grid grid-cols-2 gap-1">
+                <Checkbox bind:value={$fine_tune_model} label="Grid search" check="checkbox" />
+                <Checkbox bind:value={$cross_validation} label="Cross validation" check="checkbox" />
                 <CustomSelect
-                    label="backend"
-                    items={backends}
-                    bind:value={$parallel_computation_backend}
-                    disabled={!$parallel_computation}
+                    items={grid_search_methods}
+                    bind:value={$grid_search_method}
+                    disabled={!$fine_tune_model}
                 />
-            </div>
-            <Checkbox bind:value={$skip_invalid_y_values} label="skip_invalid_y_values" check="checkbox" />
-        </div>
+                <CustomInput
+                    bind:value={$cv_fold}
+                    min="2"
+                    type="number"
+                    disabled={!($cross_validation || $fine_tune_model)}
+                />
+                <CustomInput
+                    label="n_iter"
+                    bind:value={$randomzied_gridsearch_niter}
+                    min="2"
+                    type="number"
+                    disabled={!$fine_tune_model}
+                />
+                <CustomInput
+                    label="Halving factor"
+                    bind:value={$halving_factor}
+                    min="2"
+                    type="number"
+                    disabled={!$fine_tune_model}
+                />
+            </Content>
+        </Paper>
+        <Paper transition {elevation} style={paper_style}>
+            <Title>Augment data-size</Title>
+            <Content>
+                <Checkbox bind:value={$bootstrap} label="bootstrap" check="checkbox" />
+                <div class="flex gap-2">
+                    <CustomInput
+                        label="n_samples"
+                        bind:value={$bootstrap_nsamples}
+                        type="number"
+                        step="1"
+                        disabled={!$bootstrap}
+                    />
+
+                    <CustomInput
+                        label="noise (%)"
+                        bind:value={$noise_percentage}
+                        type="number"
+                        step="0.1"
+                        min="0"
+                        max="100"
+                        disabled={!$bootstrap}
+                    />
+                </div>
+            </Content>
+        </Paper>
+        <Paper transition {elevation} style={paper_style}>
+            <Title>Parallel processing</Title>
+            <Content>
+                <Checkbox bind:value={$parallel_computation} label="Parallel computation" check="checkbox" />
+                <div class="flex gap-2 items-end">
+                    <CustomInput
+                        label="n_jobs"
+                        bind:value={$n_jobs}
+                        type="number"
+                        step="1"
+                        disabled={!$parallel_computation}
+                    />
+                    <CustomSelect
+                        label="backend"
+                        items={backends}
+                        bind:value={$parallel_computation_backend}
+                        disabled={!$parallel_computation}
+                    />
+                </div>
+                <Checkbox bind:value={$skip_invalid_y_values} label="skip_invalid_y_values" check="checkbox" />
+            </Content>
+        </Paper>
     </div>
 </CustomPanel>
