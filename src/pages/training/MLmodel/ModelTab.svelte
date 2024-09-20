adc913674434e59e282194713b66bef0c372f0cc
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-20 16:47:54 -0400

    Update ModelTab.svelte

--- src/pages/training/MLmodel/ModelTab.svelte
+++ src/pages/training/MLmodel/ModelTab.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
+    import Tab, { Label } from '@smui/tab';
+    import TabBar from '@smui/tab-bar';
     import { model } from './stores';
     import supervised_ml_models from '$lib/config/ml_model/ml_models_parameters';
     import { typeSafeObjectKeys } from '$lib/utils';
 </script>
 
-<div role="tablist" class="tabs tabs-bordered flex flex-wrap">
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <!-- svelte-ignore a11y-missing-attribute -->
-    {#each typeSafeObjectKeys(supervised_ml_models) as tab (tab)}
-        <!-- svelte-ignore a11y-interactive-supports-focus -->
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <a role="tab" class="tab" class:tab-active={$model === tab} on:click={() => ($model = tab)}>
-            {tab.toLocaleUpperCase()}
-        </a>
-    {/each}
+<div class="flex flex-wrap w-max">
+    <TabBar tabs={typeSafeObjectKeys(supervised_ml_models)} let:tab bind:active={$model}>
+        <Tab {tab}>
+            <Label>{tab}</Label>
+        </Tab>
+    </TabBar>
 </div>
