4e86af3feb1a9d5fba0403c980554bb6741530fa
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-20 19:09:51 -0400

    Update Nav.svelte

--- src/lib/layouts/Nav.svelte
+++ src/lib/layouts/Nav.svelte
@@ -3,12 +3,10 @@
     let tabs = ['Home', 'Training', 'Settings'];
 </script>
 
-<div class="tabs tabs-boxed bg-orange-300/50 shadow-xl rounded-0 flex">
+<div class="flex tabs tabs-boxed bg-orange-300/50 shadow-xl">
     {#each tabs as tab (tab)}
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <!-- svelte-ignore a11y-no-static-element-interactions -->
-        <div class="tab" class:tab-active={$active_tab === tab} on:click={() => ($active_tab = tab)}>
+        <button class="tab" class:tab-active={$active_tab === tab} on:click={() => ($active_tab = tab)}>
             {tab}
-        </div>
+        </button>
     {/each}
 </div>
