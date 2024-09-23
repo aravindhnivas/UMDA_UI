cf7ff367a1252c3d5e095bc7adbe25e63a50e86b
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-22 23:55:13 -0400

    Update CustomInput.svelte

--- src/lib/components/CustomInput.svelte
+++ src/lib/components/CustomInput.svelte
@@ -1,25 +1,24 @@
 <script lang="ts">
     import { UnlockKeyhole, LockKeyhole } from 'lucide-svelte/icons';
-    export let label: string = '';
+
+    export let id: string = getID(5);
     export let value: string | number;
+    export let label: string = '';
     export let helper: string = '';
     export let helperHighlight: string = '';
-    export let enabled_lock_mode = false;
-    export let lock: boolean = false;
+    export let lock: boolean | null = null;
     export let disabled = false;
-    export let id: string = getID(5);
 
     let className = '';
     export { className as class };
-    // export let helper_class = '';
 
-    $: element_disabled = disabled || (enabled_lock_mode && lock);
+    $: element_disabled = disabled || lock;
 </script>
 
 <div class="grid gap-1 {className}">
-    <div class="flex gap-1 items-center">
-        {#if label}
-            {#if enabled_lock_mode}
+    {#if label}
+        <div class="flex items-center gap-1">
+            {#if lock !== null}
                 <button on:click={() => (lock = !lock)}>
                     {#if lock}
                         <LockKeyhole size="20" class="text-gray-700" />
@@ -28,30 +27,34 @@
                     {/if}
                 </button>
             {/if}
-            <span class="text-xs pl-1 {element_disabled ? 'text-gray-600/75' : ''}">{label}</span>
-        {/if}
+            <span class="text-xs {element_disabled ? 'text-gray-600/75' : ''}">
+                {label}{element_disabled ? ' (locked)' : ''}
+            </span>
+        </div>
+    {/if}
+    <div class="flex items-center gap-2">
+        <input
+            {id}
+            class="w-full input input-sm {element_disabled ? 'bg-gray-600/25' : ''}"
+            bind:value
+            {...$$restProps}
+            on:change
+            on:keydown
+            disabled={element_disabled}
+            autocomplete="off"
+            autocapitalize="off"
+            autocorrect="off"
+        />
     </div>
-    <input
-        {id}
-        class=" input input-sm {element_disabled ? 'bg-gray-600/25' : ''}"
-        bind:value
-        {...$$restProps}
-        on:change
-        on:keydown
-        disabled={element_disabled}
-        autocomplete="off"
-        autocapitalize="off"
-        autocorrect="off"
-    />
+
     {#if helper}
-        <span class="text-xs text-wrap break-words {element_disabled ? 'text-gray-600/75' : ''}">
-            {helper}
+        <div class="grid grid-cols-[1fr_auto] gap-1 text-xs text-wrap break-words">
+            <span class={element_disabled ? 'text-gray-600/75' : ''}>{helper}</span>
             {#if helperHighlight}
-                <br />
                 <div class="badge badge-sm badge-neutral {element_disabled ? 'text-gray' : ''}">
                     {helperHighlight}
                 </div>
             {/if}
-        </span>
+        </div>
     {/if}
 </div>
