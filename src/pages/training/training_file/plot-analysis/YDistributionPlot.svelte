38a2424701646deb169e20b606bd7b2f361a35f3
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-23 21:14:40 -0400

    Update stores.ts

--- src/pages/training/training_file/plot-analysis/YDistributionPlot.svelte
+++ src/pages/training/training_file/plot-analysis/YDistributionPlot.svelte
@@ -118,10 +118,9 @@
             $min_yvalue = String(data.descriptive_statistics.min);
             $max_yvalue = String(data.descriptive_statistics.max);
 
-            // if (auto_transform_data) {
-            applied_transformation = data?.applied_transformation || '';
-            $ytransformation = applied_transformation || 'None';
-            // }
+            applied_transformation = data?.applied_transformation || 'None';
+            $ytransformation = applied_transformation;
+
             const histogramTrace: Partial<Plotly.PlotData> = {
                 x: data.histogram.bin_edges.slice(0, -1),
                 y: data.histogram.counts,
