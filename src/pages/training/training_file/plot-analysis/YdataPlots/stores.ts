7b092bb8417a23ebfeaba71f5e21d8b35e791557
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-09-28 11:08:53 -0400

    Update stores.ts

--- src/pages/training/training_file/plot-analysis/YdataPlots/stores.ts
+++ src/pages/training/training_file/plot-analysis/YdataPlots/stores.ts
@@ -2,19 +2,22 @@ import { current_post_analysis_files_directory } from '../stores';
 
 export const ytransformation = writable<string>('None');
 
-const y_data_distribution_savefilename = 'y_data_distribution';
-export const savefilename = derived(ytransformation, $ytransformation => {
-    if ($ytransformation == 'None') return `${y_data_distribution_savefilename}.json`;
-    return `${y_data_distribution_savefilename}_for_${$ytransformation}_transformation.json`;
-});
+export const y_data_distribution_savefilename = writable<string>('y_data_distribution');
+export const savefilename = derived(
+    [ytransformation, y_data_distribution_savefilename],
+    ([$ytransformation, $y_data_distribution_savefilename]) => {
+        if ($ytransformation == 'None') return `${$y_data_distribution_savefilename}.json`;
+        return `${$y_data_distribution_savefilename}_for_${$ytransformation}_transformation.json`;
+    },
+);
 
 export const min_yvalue = writable<string>('');
 export const max_yvalue = writable<string>('');
 
 export const save_loc_for_ydistribution_data = derived(
-    current_post_analysis_files_directory,
-    async $current_post_analysis_files_directory => {
+    [current_post_analysis_files_directory, y_data_distribution_savefilename],
+    async ([$current_post_analysis_files_directory, $y_data_distribution_savefilename]) => {
         const analysis_dir = await $current_post_analysis_files_directory;
-        return await path.join(analysis_dir, 'y_data_distribution');
+        return await path.join(analysis_dir, $y_data_distribution_savefilename);
     },
 );
