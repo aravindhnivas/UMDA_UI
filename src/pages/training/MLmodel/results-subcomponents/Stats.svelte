06293facf053fdd1d56744531548aa0f339fd775
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-10-04 19:29:12 -0400

    Update Stats.svelte

--- src/pages/training/MLmodel/results-subcomponents/Stats.svelte
+++ src/pages/training/MLmodel/results-subcomponents/Stats.svelte
@@ -4,25 +4,33 @@
     export let cv_fold: number | undefined;
     export let significant_digits: number = 2;
     export let bg_color: 'warning' | 'info' = 'warning';
+
+    const metrics = ['r2', 'mse', 'rmse', 'mae'] as const;
 </script>
 
 <div class="grid grid-cols-6 gap-2 items-center w-3xl">
     <span class="badge badge-{bg_color} col-span-2"><slot /></span>
-    <span class="badge badge-{bg_color}">{stats.r2.toFixed(significant_digits)}</span>
-    <span class="badge badge-{bg_color}">{stats.mse.toFixed(significant_digits)}</span>
-    <span class="badge badge-{bg_color}">{stats.rmse.toFixed(significant_digits)}</span>
-    <span class="badge badge-{bg_color}">{stats.mae.toFixed(significant_digits)}</span>
+    {#each metrics as metric}
+        {#if stats[metric]}
+            <span class="badge badge-{bg_color}">{stats[metric].toFixed(significant_digits)}</span>
+        {:else}
+            <span class="badge badge-error">-</span>
+        {/if}
+    {/each}
 </div>
 {#if rcv}
     <div class="grid grid-cols-6 gap-2 items-center w-3xl">
         <span class="badge col-span-2">{cv_fold}-fold CV:</span>
-        <span class="badge">{rcv.r2.mean.toFixed(significant_digits)} ({rcv.r2.std.toFixed(significant_digits)})</span>
-        <span class="badge">{rcv.mse.mean.toFixed(significant_digits)} ({rcv.mse.std.toFixed(significant_digits)})</span
-        >
-        <span class="badge"
-            >{rcv.rmse.mean.toFixed(significant_digits)} ({rcv.rmse.std.toFixed(significant_digits)})</span
-        >
-        <span class="badge">{rcv.mae.mean.toFixed(significant_digits)} ({rcv.mae.std.toFixed(significant_digits)})</span
-        >
+        {#each metrics as metric}
+            {#if rcv[metric]}
+                {@const mean = rcv[metric].mean.toFixed(significant_digits)}
+                {@const std = rcv[metric].std.toFixed(significant_digits)}
+                {#if mean && std}
+                    <span class="badge">{mean} ({std})</span>
+                {/if}
+            {:else}
+                <span class="badge badge-error">-</span>
+            {/if}
+        {/each}
     </div>
 {/if}
