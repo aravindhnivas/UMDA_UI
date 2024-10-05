7acd524f951fe5987b3cf15485c55bfc37793d55
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-10-04 23:56:28 -0400

    Update OptunaTuneInput.svelte

--- src/lib/components/OptunaTuneInput.svelte
+++ src/lib/components/OptunaTuneInput.svelte
@@ -3,13 +3,12 @@
     import { SlidersHorizontal } from 'lucide-svelte/icons';
 
     export let value: string | number | boolean = 'name';
-    export let items: string[] | null = null;
+    export let items: string[] | null = ['name', 'name2'];
+    // export let value: string | number | boolean = true;
+    // export let items: string[] | null = null;
     export let label: string = 'label';
     export let description: string = 'description';
-    export let low: string | number = '';
-    export let high: string | number = '';
-    export let step: string | number = '';
-    export let type: 'int' | 'float' | 'categorical' = 'float';
+    export let type: 'int' | 'float' = 'float';
     export let scale: 'log' | '' = 'log';
     export let lock: boolean = false;
     export let fine_tune: boolean = false;
@@ -18,11 +17,10 @@
     let component: 'input' | 'select' | 'checkbox';
     onMount(() => {
         if (isArray(items) && items.length > 0) {
-            type = 'categorical';
-            fine_tuned_values = items.join(', ');
+            fine_tuned_values ||= items.join(', ');
         } else if (isBoolean(value)) {
-            fine_tuned_values = 'true, false';
             component = 'checkbox';
+            fine_tuned_values = 'true, false';
         } else if (isString(value) || isNumber(value)) {
             component = 'input';
         }
@@ -35,43 +33,28 @@
         <span class="text-sm {lock ? 'text-gray-600/75' : ''}">{label}</span>
     </span>
     {#if fine_tune}
-        {#if type === 'categorical' || items?.length}
-            <input
-                class="input input-bordered input-sm join-item col-span-5"
-                placeholder="categories"
-                bind:value={fine_tuned_values}
-                disabled={lock}
-            />
-        {:else if component === 'input'}
-            <input class="input input-bordered input-sm join-item" placeholder="low" bind:value={low} disabled={lock} />
-            <input
-                class="input input-bordered input-sm join-item"
-                placeholder="high"
-                bind:value={high}
-                disabled={lock}
-            />
-            <input
-                class="input input-bordered input-sm join-item"
-                placeholder="step"
-                bind:value={step}
-                disabled={lock}
-            />
-        {:else if component === 'checkbox'}
+        {#if component === 'checkbox'}
             <input
                 class="input input-bordered input-sm join-item col-span-5"
                 bind:value={fine_tuned_values}
                 disabled={true}
             />
+        {:else}
+            <input
+                class="input input-bordered input-sm join-item {component === 'input' ? 'col-span-3' : 'col-span-5'}"
+                placeholder="min, max, step"
+                bind:value={fine_tuned_values}
+                disabled={lock}
+            />
         {/if}
-        {#if type === 'float' && component === 'input'}
-            <select class="select select-bordered select-sm join-item" bind:value={scale} disabled={lock}>
-                <option disabled selected>type</option>
-                <option>log</option>
-                <option></option>
-            </select>
-        {/if}
-
         {#if component === 'input'}
+            {#if type === 'float'}
+                <select class="select select-bordered select-sm join-item" bind:value={scale} disabled={lock}>
+                    <option disabled selected>type</option>
+                    <option>log</option>
+                    <option></option>
+                </select>
+            {/if}
             <select
                 class="select select-bordered select-sm join-item"
                 class:col-span-2={type !== 'float'}
@@ -103,7 +86,6 @@
             disabled={lock}
         />
     {/if}
-
     <button
         disabled={lock}
         class="flex gap-1 items-center btn btn-sm btn-square join-item"
