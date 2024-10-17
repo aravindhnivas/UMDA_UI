29d9e7714af93e96a3a65b3efd9b91fd15981a72
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-10-17 15:44:58 -0400

    Update MLmodelTrain.svelte

--- src/pages/training/MLmodel/MLmodelTrain.svelte
+++ src/pages/training/MLmodel/MLmodelTrain.svelte
@@ -226,11 +226,11 @@
             }
         }
 
-        const optuna_storage = await $optuna_storage_file;
-        if ($fine_tune_model && $grid_search_method === 'Optuna' && !(await fs.exists(optuna_storage))) {
-            toast.error('Error: Optuna storage file not provided');
-            return;
-        }
+        // const optuna_storage = await $optuna_storage_file;
+        // if ($fine_tune_model && $grid_search_method === 'Optuna' && !(await fs.exists(optuna_storage))) {
+        //     toast.error('Error: Optuna storage file not provided');
+        //     return;
+        // }
 
         const args = {
             model: $model,
@@ -273,7 +273,7 @@
             learning_curve_train_sizes,
             analyse_shapley_values: $analyse_shapley_values,
             optuna_resume_study: $optuna_resume_study,
-            optuna_storage_file: optuna_storage,
+            optuna_storage_file: await $optuna_storage_file,
         };
 
         delete $results[$model];
