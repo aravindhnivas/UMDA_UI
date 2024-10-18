07d027fefb0ba93c5a3fc9c2070afab465b43831
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-10-17 22:13:42 -0400

    Update FileExists.svelte

--- src/lib/components/FileExists.svelte
+++ src/lib/components/FileExists.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
     export let name: string | Promise<string>;
+
+    const get_dir_and_basename = async (name: string | Promise<string>) => {
+        const resolved = await name;
+        const file_exists = await fs.exists(resolved);
+        const dirname = await path.dirname(resolved);
+        const basename = await path.basename(resolved);
+        return { resolved, file_exists, dirname, basename };
+    };
 </script>
 
-{#await name then resolved}
-    {#await fs.exists(resolved) then name_exists}
-        {#if name_exists}
-            <slot {resolved} />
-        {/if}
-    {/await}
+<!-- {#await name then resolved} -->
+{#await get_dir_and_basename(name) then { resolved, file_exists, dirname, basename }}
+    {#if file_exists}
+        <slot {resolved} {dirname} {basename} />
+    {:else}
+        <slot name="else" {resolved} {dirname} {basename} />
+    {/if}
 {/await}
+<!-- {/await} -->
