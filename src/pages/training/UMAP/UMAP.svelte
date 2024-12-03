09d551ca3e1831c2f2e14f5ab0197ffa1b2d6593
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-12-03 13:11:49 -0500

    Create UMAP.svelte

---
+++ src/pages/training/UMAP/UMAP.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import LoadedFileInfos from '../embedding/LoadedFileInfos.svelte';
+
+    export let id: string = 'umap-embedder-container';
+    export let display: string = 'none';
+</script>
+
+<div class="grid content-start gap-2" {id} style:display>
+    <h2>UMAP - embedder</h2>
+    <LoadedFileInfos />
+</div>
